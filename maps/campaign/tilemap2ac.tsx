a5536bcac3212617a5f33202c387b617c2ff83d2
Alejocuello722 <167212967+Alejocuello722@users.noreply.github.com>
2024-05-07 18:09:43 -0500

    Add files via upload

---
+++ maps/campaign/tilemap2ac.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tilemap2" tilewidth="16" tileheight="16" tilecount="3075" columns="75">
+ <image source="../img/tilemap2.png" width="1201" height="658"/>
+</tileset>
